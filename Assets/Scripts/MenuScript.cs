e6375b49c8b0d1c936a093709bdb03df2cc874b2
Eric Karnis <ekarnis@outlook.com>
2017-04-07 21:40:08 -0400

    Added please wait message
    It's now just a black screen anymore
    Might have added some bugs around connecting to the tour guide, not sure

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -9,8 +9,11 @@ using UnityEngine.UI;
 public class MenuScript : MonoBehaviour {
 	//These are set in the inspector
 	public MediaPlayerCtrl srcMedia;
+	public GameObject video_player_controls;
+	public Text status_text;
 	public int id;
 	void Start(){
+
 	}
 
 	//This methods checks for the creation of signalling gameobjects and launches
@@ -21,15 +24,41 @@ public class MenuScript : MonoBehaviour {
 		if(GameObject.Find("VideoMessage(Clone)") != null){
 			int message_id = (int) GameObject.Find("VideoMessage(Clone)").transform.position.x;
 			int message_video = (int) GameObject.Find("VideoMessage(Clone)").transform.position.y;
-			Debug.Log (message_id + " , " +  message_video);
-			string video = "/storage/emulated/0/VRTourist/video" + message_video + ".mp4";
-				Debug.Log(message_id + " vs " + id);
+			//For android
+			string video = " file:///storage/emulated/0/VR-Tourism/video" + message_video + ".mp4";
+			//For PC, with video located in streaming assets folder
+			//string video = "video" + message_video + ".mp4";
+			Debug.Log ("Launch " +  video);
+		  Debug.Log(message_id + " vs " + id);
 			if(message_id == 0 || message_id == id){
-				srcMedia.Load (video);
+				video_player_controls.SetActive(false);
+				try{
+					srcMedia.Load(video);
+				}
+				catch(System.ApplicationException e){
+					Debug.Log("Video couldn't be loaded, exception caught");
+					video_player_controls.SetActive(true);
+					status_text.text = " Can't find Video " + message_video;
+				}
 			}
 			Destroy(GameObject.Find("VideoMessage(Clone)"));
 		}
 
+	}
+	void OnApplicationPause(bool paused){
+		if (paused == true){
+			PhotonNetwork.Disconnect();
+		}
+		else{
+			PhotonNetwork.ConnectUsingSettings("0.1");
+		}
+	}
+	void OnApplicationFocus(bool focused){
+		if (focused == true){
+			PhotonNetwork.ConnectUsingSettings("0.1");
+		}else{
+			PhotonNetwork.Disconnect();
+		}
 	}
 
 }
--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -13,13 +13,13 @@ public class NetworkedPlayer : Photon.MonoBehaviour{
 			//This sends your data to the other players in the same room
 			if (stream.isWriting){
 				stream.SendNext(PhotonNetwork.playerName.ToString());
-				Debug.Log(PhotonNetwork.playerName.ToString());
+				//Debug.Log(PhotonNetwork.playerName.ToString());
 				stream.SendNext(srcMedia.GetSeekPosition());
-				Debug.Log(srcMedia.GetSeekPosition());
+				//Debug.Log(srcMedia.GetSeekPosition());
 				stream.SendNext(srcMedia.GetDuration());
-				Debug.Log(srcMedia.GetDuration());
+				//Debug.Log(srcMedia.GetDuration());
 				stream.SendNext(srcMedia.current_video);
-				Debug.Log(srcMedia.current_video);
+				//Debug.Log(srcMedia.current_video);
 			}
 			//This recieves information from other players in the same room
 			else{
@@ -28,24 +28,5 @@ public class NetworkedPlayer : Photon.MonoBehaviour{
 				string c = (string)stream.ReceiveNext();
 			}
 		}
-/*
-	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
-		//This sends your data to the other players in the same room
-		if (stream.isWriting){
-			stream.SendNext(playerGlobal.position);
-			stream.SendNext(playerGlobal.rotation);
-			stream.SendNext(playerLocal.localPosition);
-			stream.SendNext(playerLocal.localRotation);
-		}
-		//This recieves information from other players in the same room
-		else{
-			this.transform.position = (Vector3)stream.ReceiveNext();
-			this.transform.rotation = (Quaternion)stream.ReceiveNext();
-			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
-			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
 
-			//This makes the other player's arrows point forward
-			avatar.transform.Rotate (0, 180, 0);
-		}
-	}*/
 }
